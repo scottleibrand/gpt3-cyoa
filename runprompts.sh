4783155f6d4376248f6eb90562f0e8f3dfb380f6
Scott Leibrand <scottleibrand@gmail.com>
2020-08-12 22:36:30 -0700

    for loop over dirs

--- runprompts.sh
+++ runprompts.sh
@@ -3,5 +3,8 @@
 pip3 install httpx pyyaml fire tqdm
 
 cd api
-python3 openai_api.py ../experiments/summarize-text/tomsawyer/ch1scene2/prompt.txt
+for dir in experiments/summarize-text/tomsawyer/ch1scene2/; do
+    python3 openai_api.py ../$dir/prompt.txt
+    mv output* ../$dir/
+done
 cd -

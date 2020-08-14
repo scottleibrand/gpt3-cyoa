e20fd62ca9f8094e82d3db71584bd6f0e7e32544
Scott Leibrand <scottleibrand@gmail.com>
2020-08-13 22:16:59 -0700

    s/scene/sc/

--- runprompts.sh
+++ runprompts.sh
@@ -2,17 +2,18 @@
 
 pip3 install httpx pyyaml fire tqdm
 
+# set -x
 cd api
 for experiment in summarize-text; do
     for scene in 3 4 5 6; do
-        dir=experiments/$experiment/tomsawyer/ch1scene$scene/
+        dir=experiments/$experiment/tomsawyer/ch1sc$scene/
         python3 openai_api.py ../$dir/prompt.txt
         mv output* ../$dir/
     done
 done
 for experiment in expand-text; do
     for scene in 2; do
-        dir=experiments/$experiment/tomsawyer/ch1scene$scene/
+        dir=experiments/$experiment/tomsawyer/ch1sc$scene/
         python3 openai_api.py ../$dir/prompt.txt
         mv output* ../$dir/
     done

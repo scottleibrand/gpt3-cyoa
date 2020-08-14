da3d464c7d3d5498cb7de0574ed090e116ab9085
Scott Leibrand <scottleibrand@gmail.com>
2020-08-13 21:36:17 -0700

    summarize scenes 3-6 and expand scene 2

--- runprompts.sh
+++ runprompts.sh
@@ -3,8 +3,18 @@
 pip3 install httpx pyyaml fire tqdm
 
 cd api
-for dir in experiments/summarize-text/tomsawyer/ch1scene2/; do
-    python3 openai_api.py ../$dir/prompt.txt
-    mv output* ../$dir/
+for experiment in summarize-text; do
+    for scene in 3 4 5 6; do
+        dir=experiments/$experiment/tomsawyer/ch1scene$scene/
+        python3 openai_api.py ../$dir/prompt.txt
+        mv output* ../$dir/
+    done
+done
+for experiment in expand-text; do
+    for scene in 2; do
+        dir=experiments/$experiment/tomsawyer/ch1scene$scene/
+        python3 openai_api.py ../$dir/prompt.txt
+        mv output* ../$dir/
+    done
 done
 cd -
